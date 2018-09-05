e70f1b993f7b278f781a4b953b97f1119f8fac6d
BERTHOD MARWANE p1717264 <p1717264@univ-lyon1.fr>
2018-09-05 10:10:33 +0200

    tabl commit

--- controllers/table.js
+++ controllers/table.js
@@ -7,7 +7,13 @@ app.get('/livre', function (request, response) {
 	var table = '<table>'
 	for (var i = 0, len = lesLivres.length; i < len; i++) {
 		table.concat('<tr><td>');
-		table.concat(lesLivres[i]);
+		table.concat(lesLivres[i].name);
+		table.concat('</td><td>');
+		table.concat(lesLivres[i].ISBN);
+		table.concat('</td><td>');
+		table.concat(lesLivres[i].categ);
+		table.concat('</td><td>');
+		table.concat(lesLivres[i].auteur);
 		table.concat('</td></tr>');
 	}
 	table.concat('</table>');
