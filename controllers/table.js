270087729a24120799ad8c6b5d728b9a607ade43
BERTHOD MARWANE p1717264 <p1717264@univ-lyon1.fr>
2018-09-05 10:36:20 +0200

    commentaires entete commit

--- controllers/table.js
+++ controllers/table.js
@@ -1,25 +1,33 @@
 
 
+// affichage du tableau des livres
 app.get('/livre', function (request, response) {
 	
+	// récupération des livres
 	var lesLivres = db.collection('livres').find();
 	
-	var table = '<table>';
+	// création du tableau, le titre du livre est un lien vers ce livre
+	var table = '<table><tr><th>titre</th><th>ISBN</th><th>catégorie</th><th>auteur</th></tr>'; // entête
+	//données
 	for (var i = 0, len = lesLivres.length; i < len; i++) {
 		table.concat('<tr><td>',url('/livre/', {titre: lesLivres[i].titre}),'</td><td>',lesLivres[i].ISBN,'</td><td>',
 			lesLivres[i].categ,'</td><td>',lesLivres[i].auteur,'</td></tr>');
 	}
 	table.concat('</table>');
-	res.send(table);
+	res.send(table); // envoie du tableau
 });
 
+// affichage du livre avec le titre ":titre" sous forme d'un tableau
 app.get('/livre/:titre', function (request, response) {
 	
+	// récupération du livre
 	var leLivre = db.collection('livres').findOne(titre);
 	
-	var table = '<table><tr><td>';
+	// création du tableau
+	var table = '<table><tr><th>titre</th><th>ISBN</th><th>catégorie</th><th>auteur</th></tr><tr><td>'; // entête
+	// données
 	table.concat(leLivre.titre,'</td><td>',leLivre.ISBN,'</td><td>',
 		leLivre.categ,'</td><td>',leLivre.auteur,'</td></tr></table>');
 	
-	res.send(table);
+	res.send(table); // envoie du tableau
 });
