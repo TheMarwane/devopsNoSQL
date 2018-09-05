e0b39b3676bfe89fe9bbe5a2406acd59c3fb46d5
BERTHOD MARWANE p1717264 <p1717264@univ-lyon1.fr>
2018-09-05 10:24:00 +0200

    afficheLivre commit

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,3 +1,3 @@
-n'ayant pas les droits d'administration sur l'ordinateur et aucun outil à disposition, je n'ai pas pu installer Node.js et Express
-
+n'ayant pas les droits d'administration sur l'ordinateur et aucun outil à disposition, je n'ai pas pu installer Node.js et Express.
+je n'ai donc pas pu effectuer le moindre test.
 
--- controllers/table.js
+++ controllers/table.js
@@ -2,21 +2,24 @@
 
 app.get('/livre', function (request, response) {
 	
-	var lesLivres = db.collection('livres').find()
+	var lesLivres = db.collection('livres').find();
 	
-	var table = '<table>'
+	var table = '<table>';
 	for (var i = 0, len = lesLivres.length; i < len; i++) {
-		table.concat('<tr><td>');
-		table.concat(lesLivres[i].name);
-		table.concat('</td><td>');
-		table.concat(lesLivres[i].ISBN);
-		table.concat('</td><td>');
-		table.concat(lesLivres[i].categ);
-		table.concat('</td><td>');
-		table.concat(lesLivres[i].auteur);
-		table.concat('</td></tr>');
+		table.concat('<tr><td>',lesLivres[i].titre,'</td><td>',lesLivres[i].ISBN,'</td><td>',
+			lesLivres[i].categ,'</td><td>',lesLivres[i].auteur,'</td></tr>');
 	}
 	table.concat('</table>');
 	res.send(table);
-})
+});
 
+app.get('/livre/:titre', function (request, response) {
+	
+	var leLivre = db.collection('livres').findOne(titre);
+	
+	var table = '<table><tr><td>';
+	table.concat(leLivre.titre,'</td><td>',leLivre.ISBN,'</td><td>',
+			leLivre.categ,'</td><td>',leLivre.auteur,'</td></tr></table>');
+	
+	res.send(table);
+});
