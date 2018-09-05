96c5819fd683424125fb6e8d687e049bbc01bbb1
BERTHOD MARWANE p1717264 <p1717264@univ-lyon1.fr>
2018-09-05 10:31:56 +0200

    linkTabl commit

--- controllers/table.js
+++ controllers/table.js
@@ -6,7 +6,7 @@ app.get('/livre', function (request, response) {
 	
 	var table = '<table>';
 	for (var i = 0, len = lesLivres.length; i < len; i++) {
-		table.concat('<tr><td>',lesLivres[i].titre,'</td><td>',lesLivres[i].ISBN,'</td><td>',
+		table.concat('<tr><td>',url('/livre/', {titre: lesLivres[i].titre}),'</td><td>',lesLivres[i].ISBN,'</td><td>',
 			lesLivres[i].categ,'</td><td>',lesLivres[i].auteur,'</td></tr>');
 	}
 	table.concat('</table>');
@@ -19,7 +19,7 @@ app.get('/livre/:titre', function (request, response) {
 	
 	var table = '<table><tr><td>';
 	table.concat(leLivre.titre,'</td><td>',leLivre.ISBN,'</td><td>',
-			leLivre.categ,'</td><td>',leLivre.auteur,'</td></tr></table>');
+		leLivre.categ,'</td><td>',leLivre.auteur,'</td></tr></table>');
 	
 	res.send(table);
 });
